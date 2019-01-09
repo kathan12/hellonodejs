5e7a9673c84e87f8b3a510bf70b8d0db0b7cc747
kathan12 <46467226+kathan12@users.noreply.github.com>
2019-01-09 14:28:19 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Express' });
+});
+
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'Express' });
+});
+router.post('/index', function(req, res, next) {
+const myarray=
+{
+  name :req.body.name,
+  email :req.body.email
+}
+  res.render('home', { msg: myarray });
+});
+module.exports = router;
